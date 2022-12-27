49dbe6feab992fd4f5c5be712c15fc01d6119096
shreyapandekar <113656443+shreyapandekar@users.noreply.github.com>
2022-12-27 16:36:46 +0530

    Update Transformation.cpp

--- Transformation.cpp
+++ Transformation.cpp
@@ -1,85 +1,122 @@
+
 #include <iostream>
 #include <graphics.h>
-#include<math.h>
+#include <cmath>
 using namespace std;
 
-void Matrix(int m1[3][3],int m2[3][3]){
-    int mult[3][3];
-    int i,j,k;
-
-    for(int i=0;i<3;i++)    
-    {    
-        for(j=0;j<3;j++)    
-        {    
-            mult[i][j]=0;    
-            for(k=0;k<3;k++)    
-                {    
-                mult[i][j]+=m1[i][k]*m2[k][j];    
-                }    
-        }    
-    }  
-   
-    line(mult[0][0],mult[0][1],mult[1][0],mult[1][1]);
-    line(mult[1][0],mult[1][1],mult[2][0],mult[2][1]);
-    line(mult[2][0],mult[2][1],mult[0][0],mult[0][1]);
-    delay(10000);
-};
-/*void translation(int cord_mat[3][3]){
-    float tx,ty;
-    cout<<"Enter Translation factors"<<endl;
-    cin>>tx>>ty;
-    setcolor(RED);
-    int trans_mat[3][3]={{1,0,0},{0,1,0},{tx,ty,1}};
-    Matrix(cord_mat,trans_mat);
-}   
-void scaling(int cord_mat[3][3]){
-    float sx,sy;
-    cout<<"Enter Scaling factors"<<endl;
-    cin>>sx>>sy;
-    setcolor(GREEN);
-    int scal_mat[3][3]={{sx,0,0},{0,sy,0},{0,0,1}};
-    Matrix(cord_mat,scal_mat);
-}
-*/
-void anticlockwise(float cord_mat[3][3]){
-    float rad,theta;
-    cout<<"Enter anticlockwise rotation factors"<<endl;
-    cin>>theta;
-    rad=(3.14/180)*theta;
-    float anticlk[3][3]={{cos(rad),-sin(rad),0},{sin(rad),cos(rad),0},{0,0,1}};
-    Matrix(cord_mat,anticlk);
-}  
-/*
-void clockwise(int cord_mat[3][3]){
-    float rad,theta;
-    cout<<"Enter clockwise rotation factors"<<endl;
-    cin>>theta;
-    rad=(3.14/180)*theta;
-    int clk[3][3]={{cos(rad),sin(rad),0},{-sin(rad),cos(rad),0},{0,0,1}};
-    Matrix(cord_mat,clk); 
-    */ 	
 int main()
 {
-    int gdriver=DETECT, gmode;
-    initgraph(&gdriver,&gmode,NULL);
-    int x1,y1,x2,y2,x3,y3;
-    int x1_,y1_,x2_,y2_,x3_,y3_;
-    cout<<"enter x1 and y1= ";
-    cin>>x1>>y1;
-    cout<<"enter x2 and y2= ";
-    cin>>x2>>y2;
-    cout<<"enter x3 and y3= ";
-    cin>>x3>>y3;
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, NULL);
+    int a[3][3];
+    int d;
+    cout << "enter value of x1,y1";
+    cin >> a[0][0] >> a[0][1];
+    cout << "enter value of x2 & y2";
+    cin >> a[1][0] >> a[1][1];
+    cout << "enter value of x3 & y3";
+    cin >> a[2][0] >> a[2][1];
+    a[0][2] = a[1][2] = a[2][2] = 1;
+    setcolor(RED);
+    line(a[0][0], a[0][1], a[1][0], a[1][1]);
+    line(a[1][0], a[1][1], a[2][0], a[2][1]);
+    line(a[2][0], a[2][1], a[0][0], a[0][1]);
 
-    line(x1,y1,x2,y2);
-    line(x2,y2,x3,y3);
-    line(x3,y3,x1,y1);
-    int cord_mat[3][3]={{x1,y1,1},{x2,y2,1},{x3,y3,1}};
- //   translation(cord_mat);
-   // scaling(cord_mat);
-    anticlockwise(cord_mat);
-    closegraph();
-    return 0;
+    delay(500);
+    cout << "Enter your choice\n1.Translation\n2.Scaling\n3.Rotation";
+    cin >> d;
+    switch (d)
+    {
+    case 1:
+    {
+        int b[3][3], c[3][3];
+        cout << "enter value for tx,ty";
+        cin >> b[2][0] >> b[2][1];
+        b[0][0] = b[1][1] = b[2][2] = 1;
+        b[0][1] = b[0][2] = b[1][0] = b[1][2] = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                c[i][j] = 0;
+                for (int k = 0; k < 3; k++)
+                {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        setcolor(BLUE);
+        line(c[0][0], c[0][1], c[1][0], c[1][1]);
+        line(c[1][0], c[1][1], c[2][0], c[2][1]);
+        line(c[2][0], c[2][1], c[0][0], c[0][1]);
+        delay(20000);
+    }
+    break;
+
+    case 2:
+    {
+        float sx, sy;
+        float b[3][3], c[3][3];
+        cout << "enter scaling factor Sx and Sy";
+        cin >> b[0][0] >> b[1][1];
+        b[2][2] = 1;
+        b[0][1] = b[0][2] = b[1][0] = b[1][2] = b[2][0] = b[2][1] = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                c[i][j] = 0;
+                for (int k = 0; k < 3; k++)
+                {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        setcolor(BLUE);
+        line(c[0][0], c[0][1], c[1][0], c[1][1]);
+        line(c[1][0], c[1][1], c[2][0], c[2][1]);
+        line(c[2][0], c[2][1], c[0][0], c[0][1]);
+        delay(50000);
+    }
+    break;
+
+    case 3:
+    {
+        float z, A;
+        float b[3][3];
+        float c[3][3];
+        cout << "enter the angle";
+        cin >> z;
+
+        A = (z * 3.14) / 180;
+        b[2][2] = 1;
+        b[0][2] = b[1][2] = b[2][0] = b[2][1] = 0;
+        b[0][0] = b[1][1] = cos(A);
+        b[0][1] = (-sin(A));
+        b[1][0] = sin(A);
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                c[i][j] = 0;
+                for (int k = 0; k < 3; k++)
+                {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        setcolor(BLUE);
+        line(c[0][0], c[0][1], c[1][0], c[1][1]);
+        line(c[1][0], c[1][1], c[2][0], c[2][1]);
+        line(c[2][0], c[2][1], c[0][0], c[0][1]);
+        delay(50000);
+    }
+    break;
+
+        closegraph();
+    }
 }
 
-
+/* 
